cc0e8484ec436b482bee76a43dc9cc6a80aa4d6d
Mitchell Kerubo <kerubomitchell@gmail.com>
2026-04-23 11:00:18 +0300

    update patient built

--- app/src/main/java/com/example/elitehospitalmangementsystem/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/elitehospitalmangementsystem/navigation/AppNavHost.kt
@@ -2,9 +2,11 @@ package com.example.elitehospitalmangementsystem.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.elitehospitalmangementsystem.ui.theme.SplashScreen
 import com.example.elitehospitalmangementsystem.ui.theme.screens.dashboard.DashboardScreen
 import com.example.elitehospitalmangementsystem.ui.theme.screens.forgetpassword.screens.ForgotPasswordScreen
@@ -16,7 +18,7 @@ import com.example.elitehospitalmangementsystem.ui.theme.screens.register.Regist
 
 @Composable
 fun AppNavHost(navController: NavHostController= rememberNavController(),
-                startDestination:String = ROUTE_SPLASHSCREEN){
+                startDestination:String = ROUTE_DASHBOARD){
     NavHost(navController = navController,
         startDestination = startDestination){
         composable(ROUTE_REGISTER) {RegisterScreen(navController)}
@@ -25,8 +27,21 @@ fun AppNavHost(navController: NavHostController= rememberNavController(),
         composable  (ROUTE_ADD_PATIENT) { AddPatientScreen(navController) }
         composable (ROUTE_PATIENT_LIST){ PatientListScreen(navController) }
         composable(ROUTE_FORGOT_PASSWORD) { ForgotPasswordScreen(navController)}
-        composable (ROUTE_SPLASHSCREEN){ SplashScreen(navController) }
-//        composable(ROUTE_UPDATE_PATIENT) { UpdatePatientScreen(navController) }
+        composable (ROUTE_SPLASH_SCREEN){ SplashScreen(navController) }
+        composable(ROUTE_UPDATE_PATIENT,
+            arguments = listOf(
+                navArgument("patientId"){
+                    type = NavType.StringType }
+            )){
+                backStackEntry ->
+            val patientId = backStackEntry.arguments?.getString("patientId")!!
+            UpdatePatientScreen(
+                navController = navController,
+                patientId = patientId
+            )
         }
     }
+
+    }
+
 
